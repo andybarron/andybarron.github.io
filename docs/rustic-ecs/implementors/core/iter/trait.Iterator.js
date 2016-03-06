1d0bb246946725e20e267339264235d32ded3b25
Andy Barron <AndrewLBarron@gmail.com>
2016-03-06 03:53:22 -0800

    Add docs for rustic-ecs 2.0.0

---
+++ docs/rustic-ecs/implementors/core/iter/trait.Iterator.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['recs'] = ["<a class='stability Stable' title='Stable'></a>impl&lt;'a&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/iter/trait.Iterator.html' title='core::iter::Iterator'>Iterator</a> for <a class='struct' href='recs/struct.EntityIdIter.html' title='recs::EntityIdIter'>EntityIdIter</a>&lt;'a&gt;","<a class='stability Stable' title='Stable'></a>impl&lt;'a, C&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/iter/trait.Iterator.html' title='core::iter::Iterator'>Iterator</a> for <a class='struct' href='recs/struct.EntityComponentFilter.html' title='recs::EntityComponentFilter'>EntityComponentFilter</a>&lt;'a, C&gt;","<a class='stability Stable' title='Stable'></a>impl&lt;'a&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/iter/trait.Iterator.html' title='core::iter::Iterator'>Iterator</a> for <a class='struct' href='recs/struct.ComponentIter.html' title='recs::ComponentIter'>ComponentIter</a>&lt;'a&gt;","<a class='stability Stable' title='Stable'></a>impl&lt;'a&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/iter/trait.Iterator.html' title='core::iter::Iterator'>Iterator</a> for <a class='struct' href='recs/struct.ComponentIterMut.html' title='recs::ComponentIterMut'>ComponentIterMut</a>&lt;'a&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
