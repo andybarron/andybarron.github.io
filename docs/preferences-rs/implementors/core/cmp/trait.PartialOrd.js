3b9a5baa151e1f34d122e10cd2b333a1f6ace9d8
Andy Barron <AndrewLBarron@gmail.com>
2016-03-06 22:47:25 -0800

    update preferences docs

--- docs/preferences-rs/implementors/core/cmp/trait.PartialOrd.js
+++ docs/preferences-rs/implementors/core/cmp/trait.PartialOrd.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['serde'] = ["impl&lt;'a&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/cmp/trait.PartialOrd.html' title='core::cmp::PartialOrd'>PartialOrd</a> for <a class='struct' href='serde/bytes/struct.Bytes.html' title='serde::bytes::Bytes'>Bytes</a>&lt;'a&gt;","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/cmp/trait.PartialOrd.html' title='core::cmp::PartialOrd'>PartialOrd</a> for <a class='struct' href='serde/bytes/struct.ByteBuf.html' title='serde::bytes::ByteBuf'>ByteBuf</a>",];
+implementors['rustc_serialize'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/cmp/trait.PartialOrd.html' title='core::cmp::PartialOrd'>PartialOrd</a> for <a class='enum' href='rustc_serialize/json/enum.Json.html' title='rustc_serialize::json::Json'>Json</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
